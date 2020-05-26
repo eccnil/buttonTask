7d3a2179c82b64e742b91a5490b4fa9535bcc82e
Enrique Vicent <eccnil@gmail.com>
2020-05-27 01:25:06 +0200

    final example

--- src/main.cpp
+++ src/main.cpp
@@ -11,26 +11,40 @@ auto onclick = []() {
   digitalWrite(pinGreen, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
   digitalWrite(pinGreen, LOW);
+  Serial.print("button - ");                    
+  Serial.println(xPortGetCoreID());
 };
 
 void buttonTask (void *pvParameters){
   for(;;){
     btn.timer(millis());
-    yield();
+    delay(10); //needed, now button runs too fast
   }
 }
 
 void setup() {
+  Serial.begin(115200);
   pinMode(pinBlue, OUTPUT);
   pinMode(pinGreen, OUTPUT);
   btn.init();
   btn.onClick = onclick;
-  xTaskCreate(buttonTask, "button", 10000, NULL, 1, NULL);
+  TaskHandle_t buttonTaskH;
+  
+  // simple task creation 
+  xTaskCreate(buttonTask, "button", 10000, NULL, 1, &buttonTaskH);
+
+  // creattion pinned to core (loop runs on core 1)
+  //xTaskCreatePinnedToCore(buttonTask, "button", 10000, NULL, 100, &buttonTaskH, 0);
+
+
 }
 
+//put something: delay or yield or cpu will starve
 void loop() {
-  digitalWrite(pinBlue, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(pinBlue, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+    digitalWrite(pinBlue, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(1000);                       // wait for a second
+    digitalWrite(pinBlue, LOW);    // turn the LED off by making the voltage LOW
+    delay(1000);                      // wait for a second
+    Serial.print("loop - ");                    
+    Serial.println(xPortGetCoreID());
 }
